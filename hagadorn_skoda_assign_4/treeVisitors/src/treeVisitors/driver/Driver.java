0e9a475eb1ed903f5cbe5f1db611cadc75e1c0ba
David Skoda <Dskoda1@binghamton.edu>
2016-04-12 13:01:22 -0400

    refactoring of driver a bit

--- hagadorn_skoda_assign_4/treeVisitors/src/treeVisitors/driver/Driver.java
+++ hagadorn_skoda_assign_4/treeVisitors/src/treeVisitors/driver/Driver.java
@@ -5,47 +5,17 @@ import treeVisitors.treesForStrings.Trie;
 public class Driver{
   public static void main(String args[]) {
     //Command line argument verification
-    if(args.length > 4){
-      throw new IllegalArgumentException("TreeVisitors requires four"
-          + " arguments to be passed in at runtime.\n"
-          + "More than three were passed into the execution of this program.\n"
-          + "This could be a result of extra default args set in your ant buildfile.\n" 
-          + "Ant usage: \n\t"
-          + "ant -buildfile src/build.xml run -Darg0=<input file> "
-          + "-Darg1=<output file> -Darg2=<Search String> -Darg3=<debug level>\n");
-    }
-    if(args.length < 4)
-    {
-      throw new IllegalArgumentException("StudentRecordsBackup requires four"
-          + " arguments to be passed in at runtime.\n"
-          + "You have passed in less than four arguments. Please check your usage.\n"
-          + "If you are passing four arguments and using ant, ensure that your buildfile "
-          + "has 4 arguments set in the run command. Otherwise they will silently not be passed in.\n\n"
-          + "Ant usage: \n\t"
-          + "ant -buildfile src/build.xml run -Darg0=<input file> "
-          + "-Darg1=<output file> -Darg2=<Search String> -Darg3=<debug level>\n");
-    } 
+    validateArgLength(args.length, 4);
 
+    //Verify string arguments or pass them directly to file processor here
+    
     //Verify debug level argument 
-    int debugLevel = 0;
-    try{
-      debugLevel = Integer.valueOf(args[3]);
-    }catch(NumberFormatException nfe){
-      System.err.println("Exception caught parsing argument 4: Debug Level.");
-      System.err.println("Stack Trace: " );
-      nfe.printStackTrace();
-      throw new IllegalArgumentException("Argument 4 must be a string " +
-          "that can be parsed into an int, and between 0-4, inclusive.");
-    }finally{
-      if(debugLevel < 0 || debugLevel > 4){
-        System.err.println("Debug level argument passed not in valid range.");
-        throw new IllegalArgumentException("Argument 4 must be a string " +
-            "that can be parsed into an int, and between 0-4, inclusive.");
-      } 
-    }
+    int debugLevel = validateNumParam(args[3], 0, 4, "Exception caught parsing argument 4: Debug Level.",
+        "Argument 4 must be a string that can be parsed into an int, and between 0-4, inclusive.");
     //Initialize logger
     Logger.setDebugValue(debugLevel);
 
+    //Program to an interface here?
     Trie t = new Trie();
     t.insert("Hello");
     t.insert("Herro");
@@ -58,5 +28,47 @@ public class Driver{
     System.out.println("");
     System.out.println("");
   } // end main(...)
+
+  private static void validateArgLength(int argsLength, int expectedLength)
+    throws IllegalArgumentException {
+      if(argsLength > expectedLength){
+        throw new IllegalArgumentException("TreeVisitors requires four"
+            + " arguments to be passed in at runtime.\n"
+            + "More than three were passed into the execution of this program.\n"
+            + "This could be a result of extra default args set in your ant buildfile.\n" 
+            + "Ant usage: \n\t"
+            + "ant -buildfile src/build.xml run -Darg0=<input file> "
+            + "-Darg1=<output file> -Darg2=<Search String> -Darg3=<debug level>\n");
+      }
+      if(argsLength < expectedLength)
+      {
+        throw new IllegalArgumentException("StudentRecordsBackup requires four"
+            + " arguments to be passed in at runtime.\n"
+            + "You have passed in less than four arguments. Please check your usage.\n"
+            + "If you are passing four arguments and using ant, ensure that your buildfile "
+            + "has 4 arguments set in the run command. Otherwise they will silently not be passed in.\n\n"
+            + "Ant usage: \n\t"
+            + "ant -buildfile src/build.xml run -Darg0=<input file> "
+            + "-Darg1=<output file> -Darg2=<Search String> -Darg3=<debug level>\n");
+      } 
+    }
+  private static int validateNumParam(String param, int min, int max, 
+      String exCaught, String exMessage) throws IllegalArgumentException {
+    int retVal = 0;
+    try{
+      retVal = Integer.valueOf(param);
+    }catch(NumberFormatException nfe){
+      System.err.println(exCaught);
+      System.err.println("Stack Trace: " );
+      nfe.printStackTrace();
+      throw new IllegalArgumentException(exMessage);
+    }finally{
+      if(retVal < min || retVal > max){
+        System.err.println("Argument passed not in valid range.");
+        throw new IllegalArgumentException(exMessage);
+      } 
+    }
+    return retVal;
+  }
 
 } // end public class Driver
